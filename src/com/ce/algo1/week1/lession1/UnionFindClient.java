22e81f6dde5dd611968e8be6e91e55778dbbdf4c
Piyush <mr.piyushthakkar@gmail.com>
2017-07-10 01:09:01 +0530

    algo union find

---
+++ src/com/ce/algo1/week1/lession1/UnionFindClient.java
@@ -0,0 +1,24 @@
+package com.ce.algo1.week1.lession1;
+
+import java.util.Scanner;
+
+public class UnionFindClient {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		QuickFindUF uf = new QuickFindUF(N);
+		while (!sc.hasNext())
+		{
+			int p = sc.nextInt();
+			int q = sc.nextInt();
+			if (!uf.connected(p, q))
+			{
+				uf.union(p, q);
+				System.out.println(p + " " + q);
+			}
+		}
+		sc.close();
+	}
+
+}
